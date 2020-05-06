0bc5838a237f49a5e952b75cf4bb0c3f683723c0
Irfan Ezani <irfanezani@gmail.com>
2020-05-07 02:27:52 +0800

    complete functionality

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
 function generate() {
-  const max =
+  const max = document.getElementById('max').value;
+  const min = document.getElementById('min').value;
+
+  function randomNum(number) {
+    return (Math.random() * number);
+  }
+
+  const maxNumber = randomNum(max);
+  const minNumber = randomNum(min);
+
+  const result =  Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
+
+  document.getElementById('answer').value = Math.floor(result);
+  return ;
 }
